46cd8c4afad4351413ea2bbd16ab28fa7079ec7f
Kyle Yuan <kingkyleyuan@gmail.com>
2022-04-17 18:23:31 +0800

    4/17

---
+++ pro1/build/diff_drive_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/Desktop/pro1/src/diff_drive_bot
+# Build directory: /home/parallels/Desktop/pro1/build/diff_drive_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
